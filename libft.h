37dfa9f69a3049f1547c96c678657409a578ba27
Ruben Medina Martinez <rubmedin@car2s3.42barcelona.com>
2024-12-24 18:25:26 +0100

    pipeline

---
+++ libft.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rubmedin <rubmedin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/10 23:01:47 by rubmedin          #+#    #+#             */
+/*   Updated: 2024/11/22 17:32:43 by rubmedin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#define SUCCES 0
+#define ERROR 1
+
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void 	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n); 
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
+
+
+
+#endif
